3fbc6a98708f44073fb371870929dd1e0ca3688f
Lambace <97288921+Lambace@users.noreply.github.com>
2025-12-31 10:28:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { createServer } from "http";
-import { Server } from "socket.io";
+import serverless from "serverless-http";
 
 // Import routes
 import studentsRoutes from "./routes/students.js";
@@ -13,13 +12,6 @@ import validateNisnRoutes from "./routes/validateNisn.js";
 import resultsRoutes from "./routes/resultsRoutes.js"; // pastikan nama file sesuai
 
 const app = express();
-
-// Gunakan port dari Railway (WAJIB pakai process.env.PORT)
-const PORT = process.env.PORT || 5000;
-
-const server = createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
-
 app.use(cors());
 app.use(express.json());
 
@@ -40,14 +32,5 @@ app.use("/resultsRoutes", resultsRoutes);
 // Static folder untuk foto
 app.use("/upload", express.static("upload"));
 
-// Socket.IO
-io.on("connection", (socket) => {
-  console.log("Client terhubung:", socket.id);
-});
-
-// Jalankan server
-server.listen(PORT, () => {
-  console.log(`Backend jalan di port ${PORT}`);
-});
-
-export { io };
+// Export untuk Vercel
+export default serverless(app);
