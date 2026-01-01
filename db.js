194acef7c4ad7f3e12f25d59b9f8bbb937204b66
Lambace <97288921+Lambace@users.noreply.github.com>
2026-01-01 21:24:22 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,11 @@
 import pkg from 'pg';
-
-dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  // Menggunakan connectionString dari variabel DATABASE_URL
   connectionString: process.env.DATABASE_URL,
   ssl: { 
-    rejectUnauthorized: false // Wajib ada untuk koneksi database cloud
-  },
-  connectionTimeoutMillis: 10000, // Menunggu 10 detik sebelum timeout
+    rejectUnauthorized: false 
+  }
 });
 
 export default pool;
