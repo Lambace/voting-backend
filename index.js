eb183855ad7770a6abac8d51aeab5a469763e7d5
Lambace <97288921+Lambace@users.noreply.github.com>
2026-01-03 14:35:01 +0800

    Fix CORS origin setting syntax

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const __dirname = path.dirname(__filename);
 
 // --- 1. MIDDLEWARE ---
 app.use(cors({
-  origin: '*',
+  origin: '/*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }))
