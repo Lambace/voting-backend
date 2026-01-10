b74b75eec2ab3c72e35b393406139f9694aa28aa
Lambace <97288921+Lambace@users.noreply.github.com>
2026-01-10 19:57:49 +0800

    Update db.js

--- db.js
+++ db.js
@@ -2,10 +2,8 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { 
-    rejectUnauthorized: false 
-  }
+  connectionString: "postgresql://postgres:rtoJ60l7emhtiHx5@db.hfebjulznbhhzwcjdspb.supabase.co:5432/postgres",
+  ssl: { rejectUnauthorized: false } // Supabase butuh SSL
 });
 
 export default pool;
